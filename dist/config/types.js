473050e5a1fdd63d3b3f2736600f26b3be996c98
Matias Gutierrez <matiasgutierrez225@gmail.com>
2025-06-12 23:28:57 -0300

    Agregado dist al gitignore

--- dist/config/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    RconService: Symbol.for('RconService'),
-    DiscordService: Symbol.for('DiscordService'),
-};
